4e5e5b3a01b9d4ed880c951db420027e41fbafa7
davidrianomt <91507243+davidrianomt@users.noreply.github.com>
2021-12-10 20:25:30 -0500

    Update User.java

--- src/main/java/com/negocio/app/entities/User.java
+++ src/main/java/com/negocio/app/entities/User.java
@@ -6,6 +6,7 @@
 package com.negocio.app.entities;
 
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,24 +17,23 @@ import org.springframework.data.mongodb.core.mapping.Document;
  *
  * @author David Riaño
  */
-@Document(collection = "usuarios")
+@Document(collection = "user")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
 
-    @Id
+     @Id
     private Integer id;
     private String identification;
     private String name;
+    private Date birthtDay;
+    private String monthBirthtDay;
     private String address;
     private String cellPhone;
     private String email;
     private String password;
     private String zone;
     private String type;
-    
-    //private Date birthtDay;
-    //private String monthBirthtDay;
 
 }
